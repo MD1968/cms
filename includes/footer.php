12fdd09d64003890adcde94e8575c437b8bf0f96
Mark Dyson <52258439+MD1968@users.noreply.github.com>
2020-10-19 19:21:06 +0100

    initial commit
    initial commit

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+      <!-- Footer -->
+      <footer>
+          <hr>
+          <div class="row">
+              <div class="col-lg-12">
+                  <center>
+
+                      <p>Copyright 2020 &copy; Silverheath Limited</p>
+                  </center>
+
+              </div>
+              <!-- /.col-lg-12 -->
+          </div>
+          <!-- /.row -->
+      </footer>
+
+      </div>
+      <!-- /.container -->
+
+      <!-- jQuery -->
+      <script src="js/jquery.js"></script>
+
+      <!-- Bootstrap Core JavaScript -->
+      <script src="js/bootstrap.min.js"></script>
+
+      </body>
+
+      </html>
---
+++ includes/widget.php
@@ -0,0 +1,7 @@
+   <!-- Side Widget Well -->
+                <div class="well">
+                    <h4>Welcome:</h4>
+                    <p>This is my fist attempt at a Blog. <br>Please take the time to look around.</p>
+                </div>
+
+        
