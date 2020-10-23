4ebf1cfef2d7ca46a260e0c5158328ee2bfed632
Mark Dyson <52258439+MD1968@users.noreply.github.com>
2020-10-23 21:11:52 +0100

    edit categories function added
    edit categories function added

--- admin/categories.php
+++ admin/categories.php
@@ -85,6 +85,33 @@ $cat_title = $row['cat_title'];
 
 <?php }} ?>
 
+<!-- Edit query -->
+
+<?php //UPDATE QUERY
+
+  if(isset($_POST['update_category'])){
+        $the_cat_title = $_POST['cat_title'];
+        $query = "UPDATE categories SET cat_title = '{$the_cat_title}' WHERE cat_id = {$cat_id} ";
+        $update_query = mysqli_query($connection, $query);
+        if(!$update_query ){
+
+            die("QUERY FAILED" . mysqli_error($connection));
+
+
+        }
+
+
+
+  }
+
+
+
+
+
+
+
+?>
+
 
 
 
@@ -96,7 +123,7 @@ $cat_title = $row['cat_title'];
 
             </div>
             <div class="form-group">
-            <input class="btn btn-primary" type="submit" name="submit" value="Update Category">
+            <input class="btn btn-primary" type="submit" name="update_category" value="Update Category">
             </div>
             </form>
         </div>
@@ -129,6 +156,8 @@ $cat_title = $row['cat_title'];
     echo "<tr>";
 }
 ?>
+
+<!-- Delete query -->
 
 <?php 
     if(isset($_GET['delete'])){
