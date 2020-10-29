0aec230b2390a26303781d3e9157c0cc01f19df1
Mark Dyson <52258439+MD1968@users.noreply.github.com>
2020-10-29 00:23:47 +0000

    created approve and unappove function in comments
    Comments updated

--- admin/view_all_comments.php
+++ admin/view_all_comments.php
@@ -9,8 +9,7 @@
                 <th>In Responce to</th>
                 <th>Date</th>
                 <th>Approve</th>
-                <th>Reject</th>
-                <th>Edit</th>
+                <th>Unapprove</th>
                 <th>Delete</th>
             </tr>
               <tbody >
@@ -47,12 +46,10 @@
     }
 
     echo "<td>$comment_date</td>";
+    echo "<td><a href='comments.php?approve=$comment_id'>Approve</a></td>";
+    echo "<td><a href='comments.php?unapprove=$comment_id'>Unapprove</a></td>";
 
-    echo "<td><a href='posts.php?source=edit_post&p_id='>Approve</a></td>";
-    echo "<td><a href='posts.php?delete='>Reject</a></td>";
-
-    echo "<td><a href='posts.php?source=edit_post&p_id='>Edit</a></td>";
-    echo "<td><a href='posts.php?delete='>Delete</a></td>";
+    echo "<td><a href='comments.php?delete=$comment_id'>Delete</a></td>";
    
     echo "</tr>"; 
 
@@ -64,11 +61,39 @@
 
 <?php 
 
+if(isset($_GET['approve'])){
+    
+    $the_comment_id = ($_GET['approve']);
+    
+    $query = "UPDATE comments SET comment_status = 'approved' WHERE comment_id = $the_comment_id   ";
+    $approve_comment_query = mysqli_query($connection, $query);
+    header("Location: comments.php");
+    
+    
+}
+
+
+
+
+
+if(isset($_GET['unapprove'])){
+    
+    $the_comment_id = ($_GET['unapprove']);
+    
+    $query = "UPDATE comments SET comment_status = 'unapproved' WHERE comment_id = $the_comment_id ";
+    $unapprove_comment_query = mysqli_query($connection, $query);
+    header("Location: comments.php");
+    
+    
+}
+
+
 if(isset($_GET['delete'])){
 
-    $_the_post_id = $_GET['delete'];
-$query = "DELETE FROM posts WHERE post_id = {$_the_post_id}";
+    $_the_comment_id = $_GET['delete'];
+$query = "DELETE FROM comments WHERE comment_id = {$_the_comment_id}";
 $delete_query = mysqli_query($connection, $query);
+header("Location: comments.php");
 }
 
 
